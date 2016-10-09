9dddaf8af2dc24b03c6e79462f922bac243fd6e8
Student P_03 <studentp_03@Student-P-03.local>
2016-10-09 13:23:22 +0530

    Initial Commit

---
+++ GRCoreData/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GRCoreData
+//
+//  Created by Student P_03 on 09/10/16.
+//  Copyright © 2016 Student P_03. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ GRCoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GRCoreData
+//
+//  Created by Student P_03 on 09/10/16.
+//  Copyright © 2016 Student P_03. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
